ad9a063ef4abc18c7b5bb0b69d8af5531048f34a
filippob <filippo.biscarini@gmail.com>
2025-10-09 10:07:23 +0200

    added: script to make sample sheet from folder of fastq files

---
+++ src/make_samplesheet_nosubfolder.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  6 08:29:46 2025
+
+@author: filippo
+"""
+
+## script to make a file with sample names and read file names (R1 and R2) 
+## this is written for the following file system structure
+## project folder > sample folders > R1/R2 fastq files
+
+# %% libraries 
+import pandas as pd
+from os import listdir, path
+from os.path import isfile, join
+
+# %% parameters
+mypath = 'data/prova-nosubfolder/'
+outfile = 'samplesheet.csv'
+separator = "."
+extension = ".txt"
+
+# %% samples
+print("list of files")
+fastqf = [f for f in listdir(mypath) if isfile(join(mypath, f)) if f.endswith(extension)]
+
+
+# %% fastqfiles
+print("set of samples")
+samples = [x.split(separator)[0] for x in fastqf]
+samples = set(samples)
+
+# %% sanity check
+print("N. of samples:", len(samples))
+print('N. of file pairs:', len(fastqf))
+
+# %% list of lists with paired-end files per sample
+samples = list(samples)
+files = []
+
+for s in samples:
+    temp = [x for x in fastqf if s in x]
+    files.append(sorted(temp))
+
+# %% make dataframe from list of lists
+if len(samples) == len(files):
+   df = pd.DataFrame(files, columns = ['forwardReads', 'reverseReads'])
+   df['sampleID'] = samples
+   df = df[['sampleID', 'forwardReads', 'reverseReads']]
+else:
+    print("N. of samples and n. of files do not match")
+
+
+# %% add path to file names
+df['forwardReads'] = [path.join(mypath, x) for x in df['forwardReads']]
+df['reverseReads'] = [path.join(mypath, x) for x in df['reverseReads']]
+
+# %% write out
+print("write out samplesheet file")
+
+filename = path.abspath(path.join(mypath, '..', outfile))
+print('writing to', filename)
+df.to_csv(filename, sep=',', encoding='utf-8', index=False, header=True, quotechar='"')
+
+print("DONE!")
+
